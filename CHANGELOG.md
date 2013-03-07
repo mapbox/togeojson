4e38c935151b4f8216a523f03de4452a2a9e7139
Tom MacWright <tom@macwright.org>
2013-03-07 18:12:56 -0500

    Start on 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## 0.1.0
+
+* Comments
+* GPX Support
+
+## 0.0.1
+
+* Support GeometryCollections as themselves.
--- togeojson.js
+++ togeojson.js
@@ -1,33 +1,60 @@
 toGeoJSON = (function() {
-    var removeSpace = (/\s*/g), trimSpace = (/^\s*|\s*$/g), splitSpace = (/\s+/);
+    var removeSpace = (/\s*/g),
+        trimSpace = (/^\s*|\s*$/g),
+        splitSpace = (/\s+/);
+    // generate a short, numeric hash of a string
     function okhash(x) {
         if (!x || !x.length) return 0;
         for (var i = 0, h = 0; i < x.length; i++) {
             h = ((h << 5) - h) + x.charCodeAt(i) | 0;
         } return h;
     }
+    // all Y children of X
     function get(x, y) { return x.getElementsByTagName(y); }
     function attr(x, y) { return x.getAttribute(y); }
     function attrf(x, y) { return parseFloat(attr(x, y)); }
+    // one Y child of X, if any, otherwise null
     function get1(x, y) { var n = get(x, y); return n.length ? n[0] : null; }
+    // cast array x into numbers
     function numarray(x) {
         for (var j = 0, o = []; j < x.length; j++) o[j] = parseFloat(x[j]);
         return o;
     }
+    // get the content of a text node, if any
     function nodeVal(x) { return x && x.firstChild && x.firstChild.nodeValue; }
+    // get one coordinate from a coordinate array, if any
     function coord1(v) { return numarray(v.replace(removeSpace, '').split(',')); }
+    // get all coordinates from a coordinate array as [[],[]]
     function coord(v) {
-        var coords = v.replace(trimSpace, '').split(splitSpace), o = [];
-        for (var i = 0; i < coords.length; i++) o.push(coord1(coords[i]));
+        var coords = v.replace(trimSpace, '').split(splitSpace),
+            o = [];
+        for (var i = 0; i < coords.length; i++) {
+            o.push(coord1(coords[i]));
+        }
         return o;
     }
-    function fc() { return { type: 'FeatureCollection', features: [] }; }
+
+    // create a new feature collection parent object
+    function fc() {
+        return {
+            type: 'FeatureCollection',
+            features: []
+        };
+    }
+
     t = {
         kml: function(doc, o) {
             o = o || {};
-            var gj = fc(), styleIndex = {},
+
+            var gj = fc(),
+                // styleindex keeps track of hashed styles in order to match features
+                styleIndex = {},
+                // atomic geospatial types supported by KML - MultiGeometry is
+                // handled separately
                 geotypes = ['Polygon', 'LineString', 'Point'],
-                placemarks = get(doc, 'Placemark'), styles = get(doc, 'Style');
+                // all root placemarks in the file
+                placemarks = get(doc, 'Placemark'),
+                styles = get(doc, 'Style');
 
             if (o.styles) for (var k = 0; k < styles.length; k++) {
                 styleIndex['#' + styles[k].id] = okhash(styles[k].innerHTML).toString(16);
@@ -44,19 +71,25 @@ toGeoJSON = (function() {
                         for (j = 0; j < geomNodes.length; j++) {
                             geomNode = geomNodes[j];
                             if (geotypes[i] == 'Point') {
-                                geoms.push({ type: 'Point',
+                                geoms.push({
+                                    type: 'Point',
                                     coordinates: coord1(nodeVal(get1(geomNode, 'coordinates')))
                                 });
                             } else if (geotypes[i] == 'LineString') {
-                                geoms.push({ type: 'LineString',
+                                geoms.push({
+                                    type: 'LineString',
                                     coordinates: coord(nodeVal(get1(geomNode, 'coordinates')))
                                 });
                             } else if (geotypes[i] == 'Polygon') {
-                                var rings = get(geomNode, 'LinearRing'), coords = [];
+                                var rings = get(geomNode, 'LinearRing'),
+                                    coords = [];
                                 for (k = 0; k < rings.length; k++) {
                                     coords.push(coord(nodeVal(get1(rings[k], 'coordinates'))));
                                 }
-                                geoms.push({ type: 'Polygon', coordinates: coords });
+                                geoms.push({
+                                    type: 'Polygon',
+                                    coordinates: coords
+                                });
                             }
                         }
                     }
@@ -88,18 +121,28 @@ toGeoJSON = (function() {
                         properties[simpleDatas[i].getAttribute('name')] = nodeVal(simpleDatas[i]);
                     }
                 }
-                return [{ type: 'Feature', geometry: (geoms.length === 1) ? geoms[0] : {
-                    type: 'GeometryCollection',
-                    geometries: geoms }, properties: properties }];
+                return [{
+                    type: 'Feature',
+                    geometry: (geoms.length === 1) ? geoms[0] : {
+                        type: 'GeometryCollection',
+                        geometries: geoms
+                    },
+                    properties: properties
+                }];
             }
             return gj;
         },
         gpx: function(doc, o) {
-            var i, j, tracks = get(doc, 'trk'), track, pt, gj = fc();
+            var i, j,
+                tracks = get(doc, 'trk'),
+                track,
+                pt,
+                // a feature collection
+                gj = fc();
             for (i = 0; i < tracks.length; i++) {
                 track = tracks[i];
-                var name = nodeVal(get1(track, 'name'));
-                var pts = get(track, 'trkpt'), line = [];
+                var name = nodeVal(get1(track, 'name')),
+                    pts = get(track, 'trkpt'), line = [];
                 for (j = 0; j < pts.length; j++) {
                     line.push([attrf(pts[j], 'lon'), attrf(pts[j], 'lat')]);
                 }
@@ -108,7 +151,10 @@ toGeoJSON = (function() {
                     properties: {
                         name: name || ''
                     },
-                    geometry: { type: 'LineString', coordinates: line }
+                    geometry: {
+                        type: 'LineString',
+                        coordinates: line
+                    }
                 });
             }
             return gj;
