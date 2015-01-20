ef765d072a49f7be4fd3222d7a8090c607869290
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2015-01-20 01:01:07 +0100

    Check if line.times is set
    Fixes another TypeError

--- togeojson.js
+++ togeojson.js
@@ -274,7 +274,7 @@ toGeoJSON = (function() {
                 for (var i = 0; i < segments.length; i++) {
                     line = getPoints(segments[i], 'trkpt');
                     if (line.line) track.push(line.line);
-                    if (line.times.length) times.push(line.times);
+                    if (line.times && line.times.length) times.push(line.times);
                 }
                 if (track.length === 0) return;
                 var properties = getProperties(node);
