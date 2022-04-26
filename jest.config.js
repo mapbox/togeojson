e8ad8e11dc91fe33b04fe5f82d3aa8df697c3110
Tom MacWright <tom@macwright.org>
2022-04-26 10:28:50 -0400

    Upgrade jest, switch to swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
   collectCoverage: true,
   testEnvironment: "node",
 };
